8ad27204a4427e2c02fc1a0e417d6a83b6bb2071
Vanfarock <vinicius.m.m.2002@gmail.com>
2020-11-22 14:21:11 -0300

    Project base structure

---
+++ TrabalhoFinal/src/LogicaPredicados.java
@@ -0,0 +1,29 @@
+
+import javax.swing.JPanel;
+
+public class LogicaPredicados extends Enigma {
+
+	public LogicaPredicados(String nomeArquivo) {
+		super(nomeArquivo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public JPanel mostrar() {
+		return null;
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void carregarInformacoes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void salvarInformacoes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ TrabalhoFinal/src/Teste.java
@@ -0,0 +1,6 @@
+
+public class Teste {
+	public static void main(String[] args) {
+		Enigma e = new LogicaProposicional("\\src\\enigmas\\enigma1.json");
+	}
+}
